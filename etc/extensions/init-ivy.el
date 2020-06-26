a0f48abf89c2dbde58aab8d2d92da4e2d29204b3
evanmeek <the_lty_mail@foxmail.com>
2020-06-26 18:54:36 +0800

    update

--- etc/extensions/init-ivy.el
+++ etc/extensions/init-ivy.el
@@ -69,7 +69,7 @@
     :init (ivy-posframe-mode 1) 
     :custom (ivy-posframe-parameters '((left-fringe . 8) 
                                        (right-fringe . 8))) 
-    (ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-top-center)))))
+    (ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-center)))))
 ;; counsel提供对项目管理的支持
 (use-package 
   counsel-projectile 
@@ -79,6 +79,32 @@
   :bind (:map leader-key
               ("p" . #'projectile-command-map)))
 
+;;;###autoload
+(defun ivy-telega-chat-highlight (chat)
+  (let ((unread (funcall (telega--tl-prop :unread_count) chat))
+        (title (telega-chat-title chat 'with-identity))
+        (not-muted-p (not (telega-chat-muted-p chat)))
+        (mentions (funcall (telega--tl-prop :unread_mention_count) chat)))
+
+    (if (and not-muted-p (> (+ unread mentions) 0))
+        (ivy-append-face (format "%s %d@%d" title unread mentions) 'ivy-highlight-face)
+      title)))
+
+;;;###autoload
+(defun ivy-telega-chat-with ()
+  "Starts chat with defined peer"
+  (interactive)
+
+  (telega t)
+  (let ((chats (mapcar
+                (lambda (x) (cons (ivy-telega-chat-highlight x) x))
+                (telega-filter-chats telega--ordered-chats 'all))))
+    (ivy-read "chat: " chats
+              :action (lambda (x) (telega-chat--pop-to-buffer (cdr x)))
+              :caller 'ivy-telega-chat-with)))
+(bind-key "t c" #'ivy-telega-chat-with leader-key)
+
+(setq telega-completing-read-function 'ivy-completing-read)
 
 
 (provide 'init-ivy)
