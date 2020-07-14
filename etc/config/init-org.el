2a3e0558ad3141d5c749c87c65a14a18351ebceb
evanmeek <the_lty_mail@foxmail.com>
2020-07-14 11:45:47 +0800

    修改org-capture，将其支持快速编写博客文章。注释掉README.org的过时文档

--- etc/config/init-org.el
+++ etc/config/init-org.el
@@ -4,14 +4,14 @@
   org 
   :ensure t
   :hook (('after-init-hook 'org-mode-hook)
-         ('org-mode . 'toggle-truncate-lines))
+         ;; ('org-mode . 'toggle-truncate-lines)
+		 )
   :bind
   ("C-c c" . 'org-capture)
   ("C-c a" . 'org-agenda)
   :custom
   (org-todo-keywords '((sequence "[学习](s!/@)" "[待办](t!/@)" "[等待](w!))" "|" "[完成](d!/@)" "[取消](c!@)")
-                       (sequence "[BUG](b!/@)" "[新事件](i@)" "[已知问题](k!/@)" "[修改中](W!/@)" "|" "[已修复](f!)")
-                       ))
+                       (sequence "[BUG](b!/@)" "[新事件](i@)" "[已知问题](k!/@)" "[修改中](W!/@)" "|" "[已修复](f!)")))
   (org-todo-keyword-faces '(("[学习]" . (:foreground "white" :background "#2ECC71" :weight bold))
                             ("[待办]" . (:foreground "white" :background "#F1C40F" :weight bold))
                             ("[等待]" . (:foreground "white" :background "#3498DB" :weight bold))
@@ -23,32 +23,30 @@
                             ("[修改中]" . (:foreground "white" :background "#BB8FCE" :weight bold))
                             ("[已修复]" . (:foreground "white" :background "#566573" :weight bold))))
   :config
+  (setq org-capture-templates nil)
+  ;; (setq org-time-stamp-formats '("<%Y-%m-%d 周%u %H:%M>"))
   (add-to-list 'org-capture-templates
                '("t" "任务清单"))
-  (add-to-list 'org-capture-templates
-               '("c" "代码"))
   (add-to-list 'org-capture-templates '("tw" "工作任务" entry (file+headline "~/Documents/org/capture/task.org" "Work")
                                         "* [待办] %^{任务名} - %U\n  %a\n  %?" :clock-in t :clock-keep t))
   (add-to-list 'org-capture-templates '("ts" "学习任务" entry (file+headline "~/Documents/org/capture/task.org" "Study")
                                         "* [学习] %^{学习项目} - %U\n  %a\n  %?" :clock-in t :clock-keep t))
-  (add-to-list 'org-capture-templates '("j" "记录日志" entry (file+datetree "~/Documents/org/capture/journal.org")
+  (add-to-list 'org-capture-templates '("j" "我的日志" entry (file+headline"~/Documents/site/org/diary.org" "日志")
                                         "* %U - %^{标题}\n  %?"))
-  (add-to-list 'org-capture-templates '("i" "捕获灵感" entry (file "~/Documents/org/capture/inbox.org")
+  (add-to-list 'org-capture-templates '("i" "我的闪念" entry (file+headline "~/Documents/site/org/idea.org" "闪念")
                                         "* %U - %^{标题} %^g\n  %?\n"))
-  (add-to-list 'org-capture-templates '("n" "临时笔记" entry (file "~/Documents/org/capture/notes.org")
+  (add-to-list 'org-capture-templates '("k" "我的百科" entry (file+headline "~/Documents/site/org/wiki.org" "WIKI")
                                         "* %^{标题} %t %^g\n  %?\n"))
-  (add-to-list 'org-capture-templates '("l" "超链接" entry (file+headline "~/Documents/org/capture/link.org" "Links")
-                                        "* %^{简介} %t %^g\n  %^L\n  %?\n"))
-  (add-to-list 'org-capture-templates '("cd" "代码片段" entry (file+olp "~/Documents/org/capture/code.org" "Code" "Snippets")
-                                        "* %^{简介} %t %^g\n  %a\n  #+begin_src %^{语言}\n  %?\n  #+end_src"))
-  (add-to-list 'org-capture-templates '("ct" "代码追踪" entry (file+olp "~/Documents/org/capture/code.org" "Code" "Trace")
-                                        "* %^{简介} %t %^g\n  %a\n  %?"))
-  (add-to-list 'org-capture-templates '("w" "记录单词" table-line (file+headline "~/Documents/org/capture/word.org" "Words")
+  (add-to-list 'org-capture-templates '("w" "我的单词" table-line (file+headline "~/Documents/org/capture/word.org" "Words")
                                         " | %U | %^{en_US} | %^{词性} | %^{zh_CN} |"))
-  (set-face-attribute 'org-table nil :font (font-spec :name "Sarasa Mono SC"
+  (add-to-list 'org-capture-templates '("l" "超链接" entry (file+headline "~/Documents/org/capture/link.org" "Links")
+                                        "* %^{简介} %t %^g\n  %^L\n  %?\n")))
+
+
+(set-face-attribute 'org-table nil :font (font-spec :name "Sarasa Mono SC"
                                                       :size 20
 													  ;; :style "Regular"
-													  )))
+													  ))
 
 ;; 美化org
 (use-package 
