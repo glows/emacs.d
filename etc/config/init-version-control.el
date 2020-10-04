2e69d88926abd169875d9cd39d02ca37367c9442
evanmeek <the_lty_mail@foxmail.com>
2020-10-05 06:35:33 +0800

    新增显示Git修改变化工具

--- etc/config/init-version-control.el
+++ etc/config/init-version-control.el
@@ -1,5 +1,17 @@
+;; 交互式Git工具
 (use-package magit
   :ensure t
   :commands (magit))
+
+;; 显示当前行修改-Git
+(use-package git-gutter-fringe
+  :ensure t
+  :hook (prog-mode . git-gutter-mode)
+  :custom
+  (git-gutter:update-interval 1)
+  (git-gutter:added-sign "+")
+  (git-gutter:deleted-sign "_")
+  (git-gutter:modified-sign "~")
+  (git-gutter:hide-gutter t))
 
 (provide 'init-version-control)
