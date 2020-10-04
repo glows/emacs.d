1d5a87dafe66e27a349aa240ed2f408ff75c2360
evanmeek <the_lty_mail@foxmail.com>
2020-10-04 18:04:49 +0800

    delete dired config

--- etc/extensions/init-dired.el
+++ etc/extensions/init-dired.el
@@ -1,10 +1,5 @@
 ;;; ============配置dired相关=============
 
-(use-package dired
-  :bind (:map dired-mode-map
-			  ("C-p" . dired-preview-line))
-  :config
-  (put 'dired-files-attributes-file 'disable nil))
 
 ;; 在Emacs中使用终端程序ranger
 (use-package ranger
