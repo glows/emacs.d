eaad99085bbaf82748e80452e45d1f0a28a3cc18
evanmeek <the_lty_mail@foxmail.com>
2020-10-05 06:47:34 +0800

    为org-babel运行code block添加shell支持

--- etc/config/init-org.el
+++ etc/config/init-org.el
@@ -75,7 +75,8 @@
   ;; 设置org-babel支持运行的代码
   (org-babel-do-load-languages
    'org-babel-load-languages
-   '((python . t))))
+   '((python . t)
+	 (shell . t))))
 
 (set-face-attribute 'org-table nil :font (font-spec :name evan/font-name
                                                     :size evan/font-size
