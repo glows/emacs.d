3e20720c890ed42f9aed1401e1ee4d1fa1043be0
EvanMeek <the_lty_mail@foxmail.com>
2020-10-29 00:02:36 +0800

    supplement common lisp module.

---
+++ etc/config/init-common-lisp.el
@@ -0,0 +1,17 @@
+(defvar inferior-lisp-program "/usr/bin/sbcl")
+
+;; (use-package slime
+;;   :ensure t)
+
+(use-package sly
+  :hook (common-lisp-mode . sly-edit-mode) 
+  :ensure t)
+(use-package sly-macrostep
+  :ensure t
+  :hook (common-lisp-mode . sly-macrostep-mode))
+
+(use-package sly-repl-ansi-color
+  :ensure t
+  :hook (common-lisp-mode . sly-repl-ansi-color))
+
+(provide 'init-common-lisp)
