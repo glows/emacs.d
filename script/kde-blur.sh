cb17cbc47a1788fbf2d5ee528b41106b91f66ec1
EvanMeek <the_lty_mail@foxmail.com>
2020-12-06 10:17:23 +0800

    delete someting.

--- script/kde-blur.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Works with KDE.
-
-for pid in $(pgrep emacs)
-do
-        for wid in $(xdotool search --pid $pid); do
-            xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id $wid;
-        done
-done
