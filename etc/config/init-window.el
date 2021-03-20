87f4983e27b3726d712556dbc890955675338232
EvanMeek <the_lty_mail@foxmail.com>
2021-03-20 22:31:36 +0800

    重新定义窗口规则

--- etc/config/init-window.el
+++ etc/config/init-window.el
@@ -54,62 +54,10 @@
     (advice-add #'shackle-display-buffer :around #'shackle-display-buffer-hack))
   (setq shackle-default-size 0.4
         shackle-default-alignment 'below
-        shackle-default-rule nil
-        shackle-rules
-        '((("*Help*" "*Apropos*") :select t :size 0.3 :align 'below :autoclose t)
-          (compilation-mode :select t :size 0.3 :align 'below :autoclose t)
-          (comint-mode :select t :size 0.4 :align 'below :autoclose t)
-          ("*Completions*" :size 0.3 :align 'below :autoclose t)
-          ("*Pp Eval Output*" :size 15 :align 'below :autoclose t)
-          ("*Backtrace*" :select t :size 15 :align 'below)
-          (("*Warnings*" "*Messages*") :size 0.3 :align 'below :autoclose t)
-          ("^\\*.*Shell Command.*\\*$" :regexp t :size 0.3 :align 'below :autoclose t)
-          ("\\*[Wo]*Man.*\\*" :regexp t :select t :align 'below :autoclose t)
-          ("*Calendar*" :select t :size 0.3 :align 'below)
-          (("*shell*" "*eshell*" "*ielm*") :popup t :size 0.3 :align 'below)
-          ("^\\*vc-.*\\*$" :regexp t :size 0.3 :align 'below :autoclose t)
-          ("*gud-debug*" :select t :size 0.4 :align 'below :autoclose t)
-          ("\\*ivy-occur .*\\*" :regexp t :select t :size 0.3 :align 'below)
-          (" *undo-tree*" :select t)
-          ("*quickrun*" :select t :size 15 :align 'below)
-          ("*tldr*" :size 0.4 :align 'below :autoclose t)
-          ("*osx-dictionary*" :size 20 :align 'below :autoclose t)
-          ("*Youdao Dictionary*" :size 15 :align 'below :autoclose t)
-          ("*Finder*" :select t :size 0.3 :align 'below :autoclose t)
-          ("^\\*macro expansion\\**" :regexp t :size 0.4 :align 'below)
-          ("^\\*elfeed-entry" :regexp t :size 0.7 :align 'below :autoclose t)
-          ((" *Org todo*" "*Org Dashboard*" "*Org Select*") :select t :size 0.4 :align 'below :autoclose t)
-          (" *Install vterm* " :size 0.35 :same t :align 'below)
-          (("*Paradox Report*" "*package update results*") :size 0.2 :align 'below :autoclose t)
-          ("*Package-Lint*" :size 0.4 :align 'below :autoclose t)
-          (("*Gofmt Errors*" "*Go Test*") :select t :size 0.3 :align 'below :autoclose t)
-          ("*How Do You*" :select t :size 0.5 :align 'below :autoclose t)
-
-          ("*ert*" :size 15 :align 'below :autoclose t)
-          (overseer-buffer-mode :size 15 :align 'below :autoclose t)
-
-          (" *Flycheck checkers*" :select t :size 0.3 :align 'below :autoclose t)
-          ((flycheck-error-list-mode flymake-diagnostics-buffer-mode)
-           :select t :size 0.25 :align 'below :autoclose t)
-
-          (("*lsp-help*" "*lsp session*") :size 0.3 :align 'below :autoclose t)
-          ("*DAP Templates*" :select t :size 0.4 :align 'below :autoclose t)
-          (dap-server-log-mode :size 15 :align 'below :autoclose t)
-          ("*rustfmt*" :select t :size 0.3 :align 'below :autoclose t)
-          ((rustic-compilation-mode rustic-cargo-clippy-mode rustic-cargo-outdated-mode rustic-cargo-test-mode) :select t :size 0.3 :align 'below :autoclose t)
-
-          (profiler-report-mode :select t :size 0.5 :align 'below)
-          ("*ELP Profiling Restuls*" :select t :size 0.5 :align 'below)
-
-          ((inferior-python-mode inf-ruby-mode swift-repl-mode) :size 0.4 :align 'below)
-          ("*prolog*" :size 0.4 :align 'below)
-
-          ((grep-mode rg-mode deadgrep-mode ag-mode pt-mode) :select t :size 0.4 :align 'below)
-          (Buffer-menu-mode :select t :size 20 :align 'below :autoclose t)
-          (gnus-article-mode :select t :size 0.7 :align 'below :autoclose t)
-          (helpful-mode :select t :size 0.3 :align 'below :autoclose t)
-          ((process-menu-mode cargo-process-mode) :select t :size 0.3 :align 'below :autoclose t)
-          (list-environment-mode :select t :size 0.3 :align 'below :autoclose t)
-          (tabulated-list-mode :size 0.4 :align 'below))))
+        shackle-default-rule t
+        shackle-rules '(("*lsp-ui-imenu*" :align 'left :size 0.25)
+                        (telega-chat-mode :align 'above :size 0.25)
+                        (cargo-process-mode :align 'below :size 0.25 :select t)
+                        ("*rustfmt*" :ignore t))))
 
 (provide 'init-window)
